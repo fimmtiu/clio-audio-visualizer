8ee05cf46fcbdb011e37afcb2e92eaef3a002c3e
Michael Cramm <mike.cramm@clio.com>
2019-04-11 11:38:27 -0600

    Load metrics from definitions

--- lib/clio_audio_visualizer.rb
+++ lib/clio_audio_visualizer.rb
@@ -2,8 +2,8 @@ require "clio_audio_visualizer/version"
 require "dotenv/load"
 
 require "datadog_api"
-require "clio_audio_visualizer/metrics/unauthorized_metric"
 require "clio_audio_visualizer/metrics/writer"
+require "clio_audio_visualizer/metrics/metric_loader"
 
 module ClioAudioVisualizer
 end
