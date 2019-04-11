2d46812caaacece142597312c7f6f34f143ac47d
Dennis Taylor <dennis.taylor@clio.com>
2019-04-11 13:36:07 -0700

    Write the metric variances to a file.

--- spec/metrics/writer_spec.rb
+++ spec/metrics/writer_spec.rb
@@ -1,6 +1,19 @@
 require "spec_helper"
 
 RSpec.describe Metrics::Writer do
-  subject { described_class.new("/tmp/result.json") }
+  let(:filename) { "/tmp/result.json" }
+  subject { described_class.new(filename) }
 
+  describe "#write" do
+    it "writes a well-formatted JSON file" do
+      metrics = [
+        double(key: "foo", value: -0.7),
+        double(key: "bar", value: 0.0),
+        double(key: "baz", value: 1.0),
+      ]
+      subject.write(metrics)
+      result = JSON.parse(File.read(filename))
+      expect(result).to eq metrics.map { |m| [m.key, m.value] }.to_h
+    end
+  end
 end
