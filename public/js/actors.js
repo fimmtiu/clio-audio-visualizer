6b2fc91f3c0db65f39378a896eaab4dcf96b5e2e
Chris Saunders <chris.saunders@clio.com>
2019-04-11 16:58:42 -0700

    Ocean or Forest themes to choose from

---
+++ public/js/actors.js
@@ -0,0 +1,83 @@
+function loadActors() {
+  window.actors = {}
+
+  actors["crows"] = new Howl({
+    src: ['./audio/forest/crows.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["forest"] = new Howl({
+    src: ['./audio/forest/forest_base.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["frogs_insects"] = new Howl({
+    src: ['./audio/forest/frogs_insects.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["goat_scream"] = new Howl({
+    src: ['./audio/forest/goat_scream.wav'],
+    volume: 1.0,
+    loop: false
+  });
+
+  actors["happy_goat"] = new Howl({
+    src: ['./audio/forest/happy_goat.wav'],
+    volume: 1.0,
+    loop: false
+  });
+
+  actors["monkeys_quiet"] = new Howl({
+    src: ['./audio/forest/monkeys_quiet.wav'],
+    volume: 1.0,
+    loop: true
+  });
+
+  actors["stream"] = new Howl({
+    src: ['./audio/forest/stream_base.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  // OCEAN
+
+  actors["dolphins"] = new Howl({
+    src: ['./audio/ocean/dolphins.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["man_drowning"] = new Howl({
+    src: ['./audio/ocean/man_drowning.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["ocean_beach"] = new Howl({
+    src: ['./audio/ocean/ocean_beach.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["sea_lions"] = new Howl({
+    src: ['./audio/ocean/sea_lions.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["seagulls_loop"] = new Howl({
+    src: ['./audio/ocean/seagulls_loop.wav'],
+    volume: 0.0,
+    loop: true
+  });
+
+  actors["ship_horn_oneshot"] = new Howl({
+    src: ['./audio/ocean/ship_horn_oneshot.wav'],
+    volume: 1.0,
+    loop: false
+  });
+}
--- public/js/util/poller.js
+++ public/js/util/poller.js
@@ -13,24 +13,24 @@ function getDataFromServer() {
     });
 }
 
-function mapNames(eventData) {
+function mapNames(eventData, mapper) {
   let nameOfSoundsAndScores = {};
 
   for (var k in eventData) {
-    const nameOfSound = metricsToSoundNames[k];
+    const nameOfSound = mapper[k];
     nameOfSoundsAndScores[nameOfSound] = eventData[k];
   }
   return nameOfSoundsAndScores;
 }
 
-function processVolumeChangesFromNewEvents(callback, errorCallback) {
+function processVolumeChangesFromNewEvents(callback, errorCallback, mapper) {
   getDataFromServer(errorCallback).then(function(data) {
     if (typeof data === "undefined") {
       errorCallback();
     } else {
-      const mappings = mapNames(data);
+      const mappings = mapNames(data, mapper);
       callback(mappings);
     }
-    setTimeout(processVolumeChangesFromNewEvents.bind(this, callback, errorCallback), 2000);
+    setTimeout(processVolumeChangesFromNewEvents.bind(this, callback, errorCallback, mapper), 2000);
   });
 }
