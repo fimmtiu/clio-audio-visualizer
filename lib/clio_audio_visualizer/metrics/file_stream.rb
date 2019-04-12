efd8450edde4794ac318874df1c04ed0a34d00eb
Michael Cramm <mike.cramm@clio.com>
2019-04-12 07:54:13 -0600

    Add File and Datadog stream objects that emit events.

---
+++ lib/clio_audio_visualizer/metrics/file_stream.rb
@@ -0,0 +1,32 @@
+module Metrics
+  class FileStream
+    def initialize(metric_definitions, file_path)
+      @metric_definitions = metric_definitions
+      @data = JSON.parse(File.read(file_path))
+    end
+
+    def read_data(from, to)
+      next_response_set = @data.shift
+
+      metrics = []
+      # Could probably just reduce here...
+      next_response_set.each do |response|
+        metric_definition = @metric_definitions.find { |definition| definition["metric"] == response[1]["query"] }
+        next if metric_definition.nil?
+
+        pointlist = response[1].dig("series", 0, "pointlist") || []
+        points = pointlist.select do |point|
+          Time.at(point.first / 1000).between?(from, to)
+        end
+
+        metrics << Metric2.new(metric_definition, points)
+      end
+
+      metrics
+    end
+
+    def done?
+      @data.empty?
+    end
+  end
+end
--- lib/clio_audio_visualizer/metrics/metric.rb
+++ lib/clio_audio_visualizer/metrics/metric.rb
@@ -63,4 +63,58 @@ module Metrics
       @definition["name"]
     end
   end
+
+  class Metric2
+    attr_reader :value
+
+    def initialize(definition, points)
+      @definition = definition
+      @value = process_points(points)
+    end
+
+    def datadog_query
+      @definition["metric"]
+    end
+
+    # The key this metric will be saved to in the resulting json
+    def key
+      @definition["name"]
+    end
+
+    # The function that is called when processing the results from datadog
+    def process_points(points)
+      op = @definition["metric"][0,3]
+      points.map!(&:last)
+
+      case op
+      when "sum" then points.sum
+      when "avg" then points.last
+      when "max" then points.max
+      else 0
+      end
+    end
+
+    # How far off the expected value it is. -1.0 is really, really low; 1.0 is super high.
+    # (This method is soooooooooooooooooooo ugly right now.)
+    def variance
+      expected, too_high, too_low = @definition.values_at("expected", "too_high", "too_low")
+      return 0 if value.between?(too_low, too_high)
+      difference = value - expected
+
+      # Clamp the difference between our largest and smallest allowed values.
+      max_difference = (too_high - expected) * 2
+      min_difference = (too_low - expected) * 2
+      difference = [difference, max_difference].min
+      difference = [difference, min_difference].max
+
+      # Translate the difference from a value to a float in the [-1.0, 1.0] range.
+      if value > expected
+        puts "result: #{difference.to_f / max_difference}"
+        difference.to_f / max_difference
+      else
+        puts "result: #{difference.to_f / min_difference}"
+        -(difference.to_f / min_difference)
+      end
+    end
+  end
 end
